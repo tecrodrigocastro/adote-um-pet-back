86fdebf548a9c553ec0dc3c6270cda5bf28276ab
Rodrigo Castro <rodrigo.castro@kinghost.com.br>
2024-11-20 19:40:24 -0300

    adicionando retorno padrão

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,22 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function success(mixed $data, string $message = "okay", int $statusCode = 200)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $statusCode);
+    }
+
+
+    public function error(string $message, int $statusCode = 400)
+    {
+        return response()->json([
+            'data' => null,
+            'success' => false,
+            'message' => $message,
+        ], $statusCode);
+    }
 }
