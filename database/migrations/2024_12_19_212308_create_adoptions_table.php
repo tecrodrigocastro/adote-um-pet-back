6cbf966ffb570efd66359eadc768e8defd3a69c4
Rodrigo Castro <rodrigo.castro@kinghost.com.br>
2024-12-28 10:24:30 -0300

    migrations

---
+++ database/migrations/2024_12_19_212308_create_adoptions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('adoptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pet_id')->constrained()->onDelete('cascade');
+            $table->foreignId('adopter_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
+            $table->date('adoption_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('adoptions');
+    }
+};
